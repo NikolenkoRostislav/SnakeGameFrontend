18803029db4fb608318ad2eb3600dd28d4d16875
RSlavN <rostislavnikolenkowork@gmail.com>
2025-04-14 19:46:51 +0200

    added start sequence

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ import {getDirection} from './controls.js';
 import {renderGrid} from './grid.js';
 import {moveSnake} from './snake.js';
 
-setInterval(() => {moveSnake(getDirection()); renderGrid();}, 300);
+const intro = document.getElementById("intro");
+
+renderGrid();
+document.addEventListener("keydown", startGame);
+
+
+function startGame() {
+    document.removeEventListener("keydown", startGame);
+    intro.remove();
+    setInterval(() => {moveSnake(getDirection()); renderGrid();}, 300);
+}
+
