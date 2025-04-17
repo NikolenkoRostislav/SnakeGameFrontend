1e56c4c264d3951e57fc54d763c188aa13973405
RSlavN <rostislavnikolenkowork@gmail.com>
2025-04-17 22:39:45 +0200

    Fix: fixed snake being able to go back on itself and die

--- js/controls.js
+++ js/controls.js
@@ -8,15 +8,17 @@ export const Direction = {
 };
 
 let direction = Direction.RIGHT;
+let currentDirection = direction; // Use current direction to avoid snake going back on itself
 
 function changeDirection(e) {
     const LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40;
-    if (e.keyCode === LEFT && direction != Direction.RIGHT) { direction = Direction.LEFT; }
-    if (e.keyCode === RIGHT && direction != Direction.LEFT) { direction = Direction.RIGHT; }
-    if (e.keyCode === UP && direction != Direction.DOWN) { direction = Direction.UP; }
-    if (e.keyCode === DOWN && direction != Direction.UP) { direction = Direction.DOWN; }
+    if (e.keyCode === LEFT && currentDirection != Direction.RIGHT) { direction = Direction.LEFT; }
+    if (e.keyCode === RIGHT && currentDirection != Direction.LEFT) { direction = Direction.RIGHT; }
+    if (e.keyCode === UP && currentDirection != Direction.DOWN) { direction = Direction.UP; }
+    if (e.keyCode === DOWN && currentDirection != Direction.UP) { direction = Direction.DOWN; }
 }
 
 export function getDirection() {
+    currentDirection = direction;
     return direction;
 }
