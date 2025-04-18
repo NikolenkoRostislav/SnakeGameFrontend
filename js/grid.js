830805791399262ccef0a9c5963844b841663cd3
RSlavN <rostislavnikolenkowork@gmail.com>
2025-04-18 15:19:10 +0200

    Feat: snake now starts with a random face

--- js/grid.js
+++ js/grid.js
@@ -13,14 +13,17 @@ export const CellType = {
 for (let i = 0; i < gridSize; i++) {
     const row = [];
     for (let j = 0; j < gridSize; j++) {
-        if( i === 0 || i === gridSize - 1 || j === 0 || j === gridSize - 1) {
+        if (i === 0 || i === gridSize - 1 || j === 0 || j === gridSize - 1) {
             row.push(CellType.WALL);
-        }else{
+        } else {
             row.push(CellType.FLOOR);
         }
     }
     gridData.push(row);
 }
+
+const faces = ['^_^', '*-*', '0.0', 'O_O', 'T_T', 'X_X', 'o_o', 'v_v'];
+const randomFace = faces[Math.floor(Math.random() * faces.length)];
 
 export function renderGrid() {
     grid.innerHTML = '';
@@ -29,6 +32,10 @@ export function renderGrid() {
             const div = document.createElement("div");
             div.className = cell;
             grid.appendChild(div);
+            if (cell === CellType.HEAD) {
+                div.innerHTML = `<b style="color: black; font-size: 18px; 
+                display: flex; justify-content: center; align-items: center;">${randomFace}</b>`;
+            }
         });
     });
 }
